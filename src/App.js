2375608ac278083f3e95da7e176d16e0bcfda440
Sam Singer <samuel.singer1@gmail.com>
2019-03-25 17:25:08 -0700

    Importing your first person as a component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi I'm a React App</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Bye There!'))
+    return (
+      <div className="App">
+        <h1>Hi I'm a React App</h1>
+        <p>Yay this is working!</p>
+        <Person />
+      </div>
+    );
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const person = () => {
+    return (
+        <div>
+            <h1>I'm a person too!</h1>
+        </div>
+    )
+};
+
+export default person;
