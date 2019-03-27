883caed6314997a0a31624a8d5469138c8d2182a
Sam Singer <samuel.singer1@gmail.com>
2019-03-26 17:47:22 -0700

    Using React Hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi I'm a React App</h1>
-        <p>Yay this is working!</p>
-        <Person />
-      </div>
-    );
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+  })
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        { name: 'AAA', age: 100 },
+        { name: 'BBB', age: 200 },
+        { name: 'CCC', age: 300 }
+      ]
+    })
   }
+
+  return (
+    <div className="App">
+      <h1>Keep Practicing</h1>
+      <button onClick={switchNameHandler}>Change Values</button>
+      <Person person={personsState.persons}/>
+    </div>
+  );
+
 }
 
-export default App;
+export default app;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const person = () => {
-    return (
-        <div>
-            <h1>I'm a person too!</h1>
-        </div>
-    )
+const person = (props) => {
+    return props.person.map(person => {
+        return (
+            <div>
+                <p>{person.name} and {person.age}</p>
+            </div>
+        )
+    })
 };
 
 export default person;
