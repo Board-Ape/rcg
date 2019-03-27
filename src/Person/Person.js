e5b0da2713586e0ec219d39855051ce78a0d00e0
Sam Singer <samuel.singer1@gmail.com>
2019-03-26 19:01:00 -0700

    Adding some styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return props.person.map((person, key) => {
         return (
-            <div key={key}>
+            <div className="person" key={key}>
                 <p onClick={props.click.bind(this, key)}>{person.name} and {person.age}</p>
                 <input type="text" onChange={props.changed} key={key}/>
             </div>
